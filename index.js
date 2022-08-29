8647bfd92c9914001e71b3cd27d96d0b11021e00
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-08-29 13:40:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {name, email }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    });
+}
+
+submitData();
